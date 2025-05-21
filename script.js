ec24f8e395433aafc00f2b90788de56435a84392
Baby <babymishr@example.com>
2025-05-21 12:49:21 +0530

    message

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// function toggleInfo(artistDiv) {
-//     const info = artistDiv.querySelector(".info");
+function toggleInfo(artistDiv) {
+    const info = artistDiv.querySelector(".info");
   
-//     // Hide all other infos first
-//     const allInfos = document.querySelectorAll(".info");
-//     allInfos.forEach(p => {
-//       if (p !== info) {
-//         p.style.display = "none";
-//       }
-//     });
+    // Hide all other infos first
+    const allInfos = document.querySelectorAll(".info");
+    allInfos.forEach(p => {
+      if (p !== info) {
+        p.style.display = "none";
+      }
+    });
   
-//     // Toggle current info display
-//     if (info.style.display === "block") {
-//       info.style.display = "none";
-//     } else {
-//       info.style.display = "block";
-//     }
-//   }
+    // Toggle current info display
+    if (info.style.display === "block") {
+      info.style.display = "none";
+    } else {
+      info.style.display = "block";
+    }
+  }
   
 
 
